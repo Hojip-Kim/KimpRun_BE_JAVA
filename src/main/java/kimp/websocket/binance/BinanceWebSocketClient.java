aa6617532a053cce0235b279d63a3374f327810e
Hojip-Kim <rlaghwlq12@naver.com>
2024-06-08 13:33:38 +0900

    feat : websocket handler 생성

---
+++ src/main/java/kimp/websocket/binance/BinanceWebSocketClient.java
@@ -0,0 +1,37 @@
+package kimp.websocket.binance;
+
+import lombok.extern.slf4j.Slf4j;
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.handshake.ServerHandshake;
+
+import java.net.URI;
+
+
+@Slf4j
+//@Component
+public class BinanceWebSocketClient extends WebSocketClient {
+
+    public BinanceWebSocketClient(URI serverUri){
+        super(serverUri);
+    }
+
+    @Override
+    public void onOpen(ServerHandshake handshakedata) {
+        log.info(">>> Connected to Binance Websocket Successful");
+    }
+
+    @Override
+    public void onMessage(String message) {
+        log.info("Received message : {}" , message);
+    }
+
+    @Override
+    public void onClose(int code, String reason, boolean remote) {
+        log.info(">>> DisConnected to Binance Websocket Successful");
+    }
+
+    @Override
+    public void onError(Exception ex) {
+        log.info("An error occurred: {}", ex.getMessage());
+    }
+}
