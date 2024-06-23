bdc5ccf1dae816f705b0ce8b8f07a218581cea6b
Hojip-Kim <rlaghwlq12@naver.com>
2024-06-23 22:31:17 +0900

    feat : binanceService 구현체 구

---
+++ src/main/java/kimp/market/service/serviceImpl/BinanceServiceImpl.java
@@ -0,0 +1,14 @@
+package kimp.market.service.serviceImpl;
+
+import kimp.market.dto.response.UpbitMarketList;
+import kimp.market.service.BinanceService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BinanceServiceImpl implements BinanceService {
+
+    @Override
+    public UpbitMarketList getUpbitMarketData() {
+        return null;
+    }
+}
