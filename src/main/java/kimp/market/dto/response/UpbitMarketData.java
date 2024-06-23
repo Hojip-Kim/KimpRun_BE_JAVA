72fea240ce37ec94256093061d8b848f0781f960
Hojip-Kim <rlaghwlq12@naver.com>
2024-06-23 22:43:19 +0900

    feat Upbit의 marketData ticker명, 한국, 영어이름을 담을 수 있는 DTO

---
+++ src/main/java/kimp/market/dto/response/UpbitMarketData.java
@@ -0,0 +1,31 @@
+package kimp.market.dto.response;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class UpbitMarketData {
+    @JsonProperty("market")
+    private String market;
+    @JsonProperty("korean_name")
+    private String korean_name;
+    @JsonProperty("english_name")
+    private String english_name;
+
+    public String getMarket() {
+        return market;
+    }
+
+    public String getKorean_name() {
+        return korean_name;
+    }
+
+    public String getEnglish_name() {
+        return english_name;
+    }
+}
