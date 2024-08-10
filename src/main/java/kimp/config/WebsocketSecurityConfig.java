07782c56c4b85921f4a6164f74f2631749911af7
Hojip-Kim <rlaghwlq12@naver.com>
2024-08-10 22:11:19 +0900

    feat : WebsocketSecurityConfig 파일입니다. 현재 Websocekt메시지에 대해 무조건적으로 접근을 허용하는 상태입니다. 즉, 인증 여부와 상관없이 모든 사용자는 WEbsocekt을 통해 전송되는 메시지에 접근할 수 있는 상태입니다. 현재 실시간 데이터에 대한 접근권한을 설정할 필요가 없기때문에 접근을 허용해놓은상태입니다.

---
+++ src/main/java/kimp/config/WebsocketSecurityConfig.java
@@ -0,0 +1,19 @@
+package kimp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.socket.EnableWebSocketSecurity;
+import org.springframework.security.messaging.access.intercept.MessageMatcherDelegatingAuthorizationManager.Builder;
+import org.springframework.messaging.Message;
+import org.springframework.security.authorization.AuthorizationManager;
+
+@Configuration
+@EnableWebSocketSecurity
+public class WebsocketSecurityConfig {
+
+    @Bean
+    AuthorizationManager<Message<?>> messageAuthorizationManager(Builder messages) {
+        messages.anyMessage().permitAll();
+        return messages.build();
+    }
+}
