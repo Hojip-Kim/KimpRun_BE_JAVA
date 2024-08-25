17e421175081262ce1f25d68c60a8bc0050560ba
Hojip-Kim <rlaghwlq12@naver.com>
2024-08-26 00:28:59 +0900

    feat : Upbit websocket 서버 지속 연결유지를 위한 주기적 message send 구현 및 네트워크 및 서버 장애에 의한 websocket api 외부 연결 실패 시 지속적 재연결요청을 위한 로직 구현 (주의 : 새로운 스레드를 생성하므로 synchronize에 신경쓸 것)

--- src/main/java/kimp/websocket/client/UpbitWebsocketClient.java
+++ src/main/java/kimp/websocket/client/UpbitWebsocketClient.java
@@ -1,17 +1,17 @@
 package kimp.websocket.client;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import kimp.market.service.UpbitService;
-import kimp.websocket.dto.response.SimpleUpbitDto;
+import kimp.market.components.Upbit;
+import kimp.websocket.dto.response.UpbitDto;
 import kimp.websocket.dto.response.UpbitReceiveDto;
-import kimp.websocket.TicketMessage;
-import kimp.websocket.TradeSubscribe;
+import kimp.websocket.dto.request.TicketMessage;
+import kimp.websocket.dto.request.TradeSubscribe;
 import kimp.websocket.handler.WebSocketHandler;
 import lombok.extern.slf4j.Slf4j;
 import org.java_websocket.client.WebSocketClient;
 import org.java_websocket.handshake.ServerHandshake;
+import org.springframework.scheduling.annotation.Scheduled;
 
-import java.math.BigDecimal;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.ByteBuffer;
@@ -25,22 +25,27 @@ public class UpbitWebsocketClient extends WebSocketClient {
     private final ObjectMapper objectMapper = new ObjectMapper();
 
     private final WebSocketHandler webSocketHandler;
-    private final UpbitService upbitService;
+    private final Upbit upbit;
 
-    public UpbitWebsocketClient(String serverUri, WebSocketHandler webSocketHandler, UpbitService upbitService) throws URISyntaxException {
+    private static volatile boolean isConnected = false;
+    private static volatile boolean isReconnecting = false;
+
+    public UpbitWebsocketClient(String serverUri, WebSocketHandler webSocketHandler, Upbit upbit) throws URISyntaxException {
         super(new URI(serverUri));
         this.webSocketHandler = webSocketHandler;
-        this.upbitService = upbitService;
+        this.upbit = upbit;
     }
 
     @Override
     public void onOpen(ServerHandshake handshakedata) {
         log.info(">>> Connected to Upbit Websocket Successful");
 
+        isConnected = true;
+
 
 
         try {
-            List<String> codes = upbitService.getUpbitMarketData().getMarketList();
+            List<String> codes = upbit.getMarketList().getMarkets();
             TicketMessage ticketMessage = new TicketMessage("test");
             TradeSubscribe tradeSubscribe = new TradeSubscribe("ticker", codes, false, true);
 
@@ -53,35 +58,119 @@ public class UpbitWebsocketClient extends WebSocketClient {
     }
 
     @Override
-    public void onMessage(ByteBuffer message) {
-        String receivedString = StandardCharsets.UTF_8.decode(message).toString();
-        try {
-            UpbitReceiveDto upbitReceiveDto = objectMapper.readValue(receivedString, UpbitReceiveDto.class);
+    public void onMessage(String message) {
 
-
-            SimpleUpbitDto simpleUpbitDto = new SimpleUpbitDto(upbitReceiveDto.getCode(), upbitReceiveDto.getTradeVolume(), upbitReceiveDto.getSignedChangeRate(), upbitReceiveDto.getHighest52WeekPrice(), upbitReceiveDto.getLowest52WeekPrice(), upbitReceiveDto.getOpeningPrice(), upbitReceiveDto.getTradePrice(), upbitReceiveDto.getChange(), upbitReceiveDto.getAccTradePrice24h());
-            String dtoJson = objectMapper.writeValueAsString(simpleUpbitDto);
-            webSocketHandler.sendMessageToAll(dtoJson);
-
-        } catch (Exception e) {
-            log.error("Failed to convert message to Dto", e);
-        }
     }
 
     @Override
-    public void onMessage(String message) {
-//
+    public void onMessage(ByteBuffer message) {
+
+        String receivedString = StandardCharsets.UTF_8.decode(message).toString();
+        if(!"{\"status\":\"UP\"}".equals(receivedString)) {
+            try {
+                UpbitReceiveDto upbitReceiveDto = objectMapper.readValue(receivedString, UpbitReceiveDto.class);
+
+                UpbitDto upbitDto = new UpbitDto(upbitReceiveDto.getCode().replace("KRW-", ""), upbitReceiveDto.getTradeVolume(), upbitReceiveDto.getSignedChangeRate(), upbitReceiveDto.getHighest52WeekPrice(), upbitReceiveDto.getLowest52WeekPrice(), upbitReceiveDto.getOpeningPrice(), upbitReceiveDto.getTradePrice(), upbitReceiveDto.getChange(), upbitReceiveDto.getAccTradePrice24h());
+
+                webSocketHandler.inputDataToHashMap(upbitDto);
+
+            } catch (Exception e) {
+                log.error("Failed to convert message to Dto", e);
+            }
+        }else{
+            synchronized (this) {
+                isConnected = true;
+                this.notify(); // 응답 도착하면 대기중 스레드를 꺠움
+            }
+        }
     }
 
     @Override
     public void onClose(int code, String reason, boolean remote) {
         log.info(">>> DisConnected to Upbit Websocket Successful");
+        isConnected = false;
+        attemptReConnect();
     }
 
     @Override
     public void onError(Exception ex) {
         log.error("An error occurred: " + ex.getMessage(), ex);
+        isConnected = false;
+        attemptReConnect();
+    }
+
+    /**
+     * 별도의 스레드를 추가적으로 생성하여 재연결 로직 실행
+     * 만일, 스레드를 생성하지않고 메인스레드를 사용한다면 reconnect가 되기 전까지 스레드 하나가 blocking상태로 존재할 것.
+     *
+     */
+    public void attemptReConnect(){
+        if(isReconnecting){
+            return;
+        }
+        isReconnecting = true;
+        new Thread(() -> {
+            try {
+                log.info("재연결 시도중...");
+                while (!isConnected) {
+                    try {
+                        Thread.sleep(5000);
+                        log.info("재연결 시도");
+                        this.reconnectBlocking();
+                        if (this.isOpen() && checkNetworkConnect()) {
+                            isConnected = true;
+                            log.info("재연결 성공");
+                        }else{
+                            log.warn("연결은 성공하였으나, 네트워크 상태 불안정. 네트워크 연결 다시 시도");
+                            isConnected = false;
+                        }
+                    } catch (InterruptedException e) {
+                        log.error("재 연결 방해됨.", e);
+                        Thread.currentThread().interrupt();
+                    } catch (Exception e) {
+                        log.error("재연결 실패", e);
+                    }
+                }
+            } catch (Exception ex) {
+                log.error("재연결 중 문제 발생", ex);
+            } finally {
+                if(isConnected) {
+                    isReconnecting = false;
+                }
+            }
+        }).start();
     }
 
 
+    private boolean checkNetworkConnect() {
+        try{
+            this.send("PING");
+
+            synchronized (this){
+                this.wait(10000);
+            }
+            return isConnected;
+        } catch(InterruptedException e){
+            log.error("네트워크 상태 확인 중 방해됨", e);
+            Thread.currentThread().interrupt();
+            return false;
+        }catch(Exception e){
+            log.error("네트워크 상태 확인 중 오류 발생", e);
+            return false;
+        }
+    }
+
+    @Scheduled(fixedRate = 30*1000)
+    public void sendPingMessage(){
+        if (isConnected){
+            try{
+                this.send("PING");
+                log.info("PING message set to server");
+            }catch(Exception e){
+                log.error("Failed to send PING message", e);
+            }
+        }else{
+            log.warn("Websocket server is not connected");
+        }
+    }
 }
