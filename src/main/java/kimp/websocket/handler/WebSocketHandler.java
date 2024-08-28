ad8af5caaf28e0f5e10694f6fd445a6c284a75c7
Hojip-Kim <rlaghwlq12@naver.com>
2024-08-28 22:58:24 +0900

    feat : 기존 실시간으로 데이터 변경시마다 제공하던 data를 네트워크 트래픽 조절 및 UX 향상을 위한 주기적 데이터 발송

--- src/main/java/kimp/websocket/handler/WebSocketHandler.java
+++ src/main/java/kimp/websocket/handler/WebSocketHandler.java
@@ -1,7 +1,12 @@
 package kimp.websocket.handler;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import kimp.websocket.dto.response.UpbitDto;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
@@ -14,10 +19,13 @@ import java.util.concurrent.ConcurrentHashMap;
 @RequiredArgsConstructor
 @Component
 public class WebSocketHandler extends TextWebSocketHandler {
+    private static final Logger log = LoggerFactory.getLogger(WebSocketHandler.class);
     private final ObjectMapper objectMapper;
 
     // 실시간 데이터를 받아오기위한 UpbitWebsocketClient DI
     private Map<String, WebSocketSession> sessions = new ConcurrentHashMap<>();
+
+    private Map<String, UpbitDto> dataHashMap = new ConcurrentHashMap<>();
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
@@ -37,13 +45,27 @@ public class WebSocketHandler extends TextWebSocketHandler {
         // 메시지를 받은 경우 처리 로직 (필요시)
     }
 
-    public void sendMessageToAll(String dtoJson) throws Exception {
-        for (WebSocketSession session : sessions.values()) {
-            if (session.isOpen()){
-                session.sendMessage(new TextMessage(dtoJson));
-            }
-        }
+    public void inputDataToHashMap(UpbitDto dto){
+        String key = dto.getToken();
+        dataHashMap.put(key, dto);
+    }
 
-//        System.out.println("hello");
+
+    @Async
+    @Scheduled(fixedRate = 3000)
+    public void sendMessageToAll() throws Exception {
+        try {
+            String mapToJson = objectMapper.writeValueAsString(dataHashMap);
+            log.info(String.valueOf(dataHashMap.size()));
+            dataHashMap.clear();
+            for (WebSocketSession session : sessions.values()) {
+                if (session.isOpen()) {
+                    session.sendMessage(new TextMessage(mapToJson));
+                }
+            }
+
+        }catch (Exception e){
+            log.error(e.getMessage());
+        }
     }
 }
