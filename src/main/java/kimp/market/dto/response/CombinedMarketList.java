5947546893758f4029e3a99a0c4a76bb1a63cd80
Hojip-Kim <rlaghwlq12@naver.com>
2024-08-28 22:53:07 +0900

    feat : 두 거래소의 마켓리스트를 담을 수 있는 DTO 생성

---
+++ src/main/java/kimp/market/dto/response/CombinedMarketList.java
@@ -0,0 +1,27 @@
+package kimp.market.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+public class CombinedMarketList{
+
+    @JsonProperty("firstMarketList")
+    private List<String> firstMarketList;
+
+    @JsonProperty("secondMarketList")
+    private List<String> secondMarketList;
+
+    public CombinedMarketList(List<String> firstMarketList, List<String> secondMarketList) {
+        this.firstMarketList = firstMarketList;
+        this.secondMarketList = secondMarketList;
+    }
+
+    public List<String> getFirstMarketList() {
+        return firstMarketList;
+    }
+
+    public List<String> getSecondMarketList() {
+        return secondMarketList;
+    }
+}
