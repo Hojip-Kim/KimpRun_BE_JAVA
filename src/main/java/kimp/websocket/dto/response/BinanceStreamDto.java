bd377984e809c4b5451f41fa45473ee8c3a2db9d
Hojip-Kim <rlaghwlq12@naver.com>
2024-09-02 22:31:36 +0900

    feat : Binance Stream data DTO 구현

---
+++ src/main/java/kimp/websocket/dto/response/BinanceStreamDto.java
@@ -0,0 +1,26 @@
+package kimp.websocket.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+@Getter
+public class BinanceStreamDto {
+    @JsonProperty("token")
+    private String token;
+    @JsonProperty("trade_price")
+    private BigDecimal trade_price;
+
+    public BinanceStreamDto(String token, BigDecimal trade_price) {
+        this.token = token;
+        this.trade_price = setScale(trade_price);
+    }
+
+    private BigDecimal setScale(BigDecimal input) {
+        return input.setScale(3, RoundingMode.HALF_UP);
+    }
+
+
+}
