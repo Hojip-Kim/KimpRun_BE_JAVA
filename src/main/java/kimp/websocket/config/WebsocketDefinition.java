5ac65f832226b7975f7728f290222d243e6ecaae
Hojip-Kim <rlaghwlq12@naver.com>
2024-09-02 22:36:54 +0900

    feat : WebsocketDefinition Configuration 추가 구현
    기존 Upbit에 관한 Config설정만 있던 Definition Class에 Binance도 DI 주입하였습니다.

--- src/main/java/kimp/websocket/config/WebsocketDefinition.java
+++ src/main/java/kimp/websocket/config/WebsocketDefinition.java
@@ -1,9 +1,11 @@
 package kimp.websocket.config;
 
+import kimp.market.components.Binance;
 import kimp.market.components.Upbit;
 import kimp.market.service.serviceImpl.MarketServiceImpl;
 import kimp.websocket.client.BinanceWebSocketClient;
-import kimp.websocket.handler.WebSocketHandler;
+import kimp.websocket.handler.BinanceWebsocketHandler;
+import kimp.websocket.handler.UpbitWebSocketHandler;
 import kimp.websocket.client.UpbitWebsocketClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,8 +13,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.IOException;
-import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Configuration
 @RequiredArgsConstructor
@@ -25,39 +28,31 @@ public class WebsocketDefinition {
     private String binanceWebsocketUrl;
 
     private final Upbit upbit;
-    /*
-     * @TODO
-     *   streamNames and wsUrl 환경변수설정
-     * */
+    private final Binance binance;
+
     @Bean
-    public BinanceWebSocketClient binanceWebSocketClient(MarketServiceImpl marketServiceImpl) throws URISyntaxException, IOException {
+    public BinanceWebSocketClient binanceWebSocketClient(BinanceWebsocketHandler binanceWebsocketHandler, MarketServiceImpl marketServiceImpl) throws URISyntaxException, InterruptedException, IOException {
+    List<String> binanceMarketPair = marketServiceImpl.getMarketPair(upbit, binance);
 
+        binanceMarketPair = binanceMarketPair.stream().map(data -> data.toLowerCase()).map(data -> data + "usdt@trade").collect(Collectors.toList());;
 
-        String streamNames = String.join("/",
-                "btcusdt@trade",
-                "ethusdt@trade",
-                "xrpusdt@trade",
-                "iqusdt@trade",
-                "idusdt@trade",
-                "scusdt@trade",
-                "zrxusdt@trade",
-                "pythusdt@trade"
-        );
+        String marketPairToString = binanceMarketPair.toString().replace("[", "").replace("]", "").replace(", ", "/");
+
+        String streamNames = String.join("/", marketPairToString);
 
         String wsUrl = String.format(binanceWebsocketUrl, streamNames);
-        URI uri = new URI(wsUrl);
 
-        BinanceWebSocketClient client = new BinanceWebSocketClient(uri);
-        client.connect();
+        BinanceWebSocketClient client = new BinanceWebSocketClient(wsUrl, binanceWebsocketHandler);
+        client.connectBlocking();
 
         return client;
     }
 
 
     @Bean
-    public UpbitWebsocketClient upbitWebsocketClient(WebSocketHandler webSocketHandler) throws URISyntaxException, InterruptedException {
+    public UpbitWebsocketClient upbitWebsocketClient(UpbitWebSocketHandler upbitWebSocketHandler) throws URISyntaxException, InterruptedException {
 
-        UpbitWebsocketClient client = new UpbitWebsocketClient(upbitWebsocketUrl, webSocketHandler, upbit);
+        UpbitWebsocketClient client = new UpbitWebsocketClient(upbitWebsocketUrl, upbitWebSocketHandler, upbit);
 
         client.connectBlocking();
 
