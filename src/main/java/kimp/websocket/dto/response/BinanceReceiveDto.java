4d1b8bfcc582776fcee65da4d9a9caa7640eb2e4
Hojip-Kim <rlaghwlq12@naver.com>
2024-09-02 22:31:14 +0900

    feat : Binance API를 통한 response data를 변환할 DTO 구

---
+++ src/main/java/kimp/websocket/dto/response/BinanceReceiveDto.java
@@ -0,0 +1,46 @@
+package kimp.websocket.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+
+@Getter
+public class BinanceReceiveDto {
+
+    @JsonProperty("e")
+    private String type; // event type
+    @JsonProperty("E")
+    private long time; // event time
+    @JsonProperty("s")
+    private String token; // event token
+    @JsonProperty("t")
+    private long tradeId; // trade id
+    @JsonProperty("p")
+    private BigDecimal price; // token price
+    @JsonProperty("T")
+    private long total; // Trade time
+    @JsonProperty("m")
+    private boolean isMarketMaker; // Is the buyer the market maker?
+    @JsonProperty("M")
+    private boolean Ignore; // Ignore
+    @JsonProperty("q")
+    private BigDecimal qty; // quantity
+
+    public BinanceReceiveDto(){
+
+    }
+
+    public BinanceReceiveDto(String type, long time, String token, long tradeId, BigDecimal price, long total, boolean isMarketMaker, boolean ignore, BigDecimal qty) {
+        this.type = type;
+        this.time = time;
+        this.token = token;
+        this.tradeId = tradeId;
+        this.price = price;
+        this.total = total;
+        this.isMarketMaker = isMarketMaker;
+        Ignore = ignore;
+        this.qty = qty;
+    }
+
+}
