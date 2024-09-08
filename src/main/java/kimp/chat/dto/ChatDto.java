9b6e0fe904766d9410eff400d55efb6a955328b0
Hojip-Kim <rlaghwlq12@naver.com>
2024-09-08 22:00:06 +0900

    Chore : ChatDto 구현

---
+++ src/main/java/kimp/chat/dto/ChatDto.java
@@ -0,0 +1,12 @@
+package kimp.chat.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ChatDto {
+
+    private String chatID;
+    private String content;
+}
