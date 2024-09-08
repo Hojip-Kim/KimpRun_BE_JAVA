ade7395f24cee3c0d3a2ab75de145941d64bbbd5
Hojip-Kim <rlaghwlq12@naver.com>
2024-09-08 21:56:55 +0900

    feat : collation 제거 및 registed_at index 주입 (pagination의 성능 향상을 위함)

--- src/main/java/kimp/chat/entity/Chat.java
+++ src/main/java/kimp/chat/entity/Chat.java
@@ -1,12 +1,15 @@
 package kimp.chat.entity;
 import jakarta.persistence.Id;
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 
-@Document(collation = "chat")
+@Document
+@Getter
 public class Chat {
 
     @Id
@@ -17,12 +20,13 @@ public class Chat {
     private String content;
 
     @CreatedDate
+    @Indexed
     private LocalDateTime registed_at;
-
 
     @LastModifiedDate
     private LocalDateTime updated_at;
 
+    public Chat(){};
     public Chat(String chatID, String content){
         this.chatID = chatID;
         this.content = content;
