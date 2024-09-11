a59ed24c6978d206bd352eb039d4ed0068ea9938
Hojip-Kim <rlaghwlq12@naver.com>
2024-09-11 18:43:47 +0900

    WIP : Chat Controller Test WIP
    GET : /allLog

--- src/test/java/unit/kimp/chat/controller/ChatControllerTest.java
+++ src/test/java/unit/kimp/chat/controller/ChatControllerTest.java
@@ -9,14 +9,16 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ExtendWith(MockitoExtension.class)
@@ -25,8 +27,10 @@ public class ChatControllerTest {
     @InjectMocks
     private ChatController chatController;
 
-    @MockBean
+    @Mock
     private ChatService chatService;
+
+    private MockHttpServletRequest request = new MockHttpServletRequest();
 
     private MockMvc mockMvc;
 
@@ -42,23 +46,46 @@ public class ChatControllerTest {
         // given
         List<ChatLogRequestDto> pageAndSizeList = new ArrayList<>();
 
+
         // when
         ChatLogRequestDto invalidDto1 = new ChatLogRequestDto(-1, 10); // page < 0
-        ChatLogRequestDto invalidDto2 = new ChatLogRequestDto(0, 0);   // size <= 0
+        ChatLogRequestDto invalidDto2 = new ChatLogRequestDto(10, 0);   // size <= 0
+        ChatLogRequestDto invalidDto3 = new ChatLogRequestDto(-1, 0);   // page < 0 && size <= 0
 
         // then
         assertThrows(BadRequestException.class, () -> {
-            chatController.getChats(invalidDto1, null);
+            chatController.getChats(invalidDto1, request);
         });
 
         assertThrows(BadRequestException.class, () -> {
-            chatController.getChats(invalidDto2, null);
+            chatController.getChats(invalidDto2, request);
+        });
+
+        assertThrows(BadRequestException.class, () -> {
+            chatController.getChats(invalidDto3, request);
         });
 
     }
 
-    private ChatLogRequestDto chatLogRequest(int page, int size){
-        return new ChatLogRequestDto(page, size);
+    @Test
+    @DisplayName("Get /allLog : page >= 0이거나 size > 0이면 통과")
+    void chatLogTransferSuccess() throws Exception {
+        // given
+        List<ChatLogRequestDto> pageAndSizeList = new ArrayList<>();
+
+        // when
+        ChatLogRequestDto validDto1 = new ChatLogRequestDto(0, 10); // page >= 0
+        ChatLogRequestDto validDto2 = new ChatLogRequestDto(0, 5);   // size > 1
+
+        // then
+
+        assertDoesNotThrow(()-> {
+            chatController.getChats(validDto1, request);
+        });
+
+        assertDoesNotThrow(()-> {
+            chatController.getChats(validDto2, request);
+        });
     }
 
 }
