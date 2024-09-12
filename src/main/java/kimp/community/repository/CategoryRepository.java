2848d8afee53f8c9e7e46b715d670c5869a77e60
Hojip-Kim <rlaghwlq12@naver.com>
2024-09-12 17:40:31 +0900

    feat : Category Repository 구현
    findByCategoryName 메서드 추가

---
+++ src/main/java/kimp/community/repository/CategoryRepository.java
@@ -0,0 +1,14 @@
+package kimp.community.repository;
+
+import kimp.community.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    public Optional<Category> findByCategoryName(String name);
+
+}
