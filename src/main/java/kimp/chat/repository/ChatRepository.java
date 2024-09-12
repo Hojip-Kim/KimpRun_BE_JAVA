70fbb94a97847ff30a9ebeca3cbfc37cdc5cd84b
Hojip-Kim <rlaghwlq12@naver.com>
2024-09-12 17:50:10 +0900

    feat : persistence layer 명시를 위한 Repository annotation 명명 (JpaRepository 내부에 Repository annotation이 있지만, 명시적 구성)

--- src/main/java/kimp/chat/repository/ChatRepository.java
+++ src/main/java/kimp/chat/repository/ChatRepository.java
@@ -5,9 +5,11 @@ import kimp.chat.entity.Chat;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ChatRepository extends MongoRepository<Chat, String> {
 
 
