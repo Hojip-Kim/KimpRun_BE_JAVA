cae8271298cf62e24a94a68233e6197f130e29e2
Hojip-Kim <rlaghwlq12@naver.com>
2024-09-13 13:43:50 +0900

    feat : Request Json Mapping을 위한 생성자 및 Setter추가

--- src/main/java/kimp/user/dto/request/CreateUserDTO.java
+++ src/main/java/kimp/user/dto/request/CreateUserDTO.java
@@ -1,8 +1,14 @@
 package kimp.user.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateUserDTO {
     private String loginId;
     private String password;
