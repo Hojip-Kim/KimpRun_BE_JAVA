a825c2dd43f4f1d95dda1b6850a5cf02c61ef4a8
Hojip-Kim <rlaghwlq12@naver.com>
2024-09-13 09:48:58 +0900

    feat : UserDto 구현 - password를 제외한 Dto

---
+++ src/main/java/kimp/user/dto/UserDto.java
@@ -0,0 +1,17 @@
+package kimp.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class UserDto {
+
+    private String loginId;
+
+    private String nickname;
+
+
+}
