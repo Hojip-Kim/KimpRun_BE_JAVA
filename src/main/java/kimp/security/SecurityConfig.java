79cb246ca89d702233ab604863b2f415dfe08e31
Hojip-Kim <rlaghwlq12@naver.com>
2024-09-20 17:55:02 +0900

    refactor : custom success/failure 의존성주입 및 authentication manager 의존성 주입, cors handling 방식 변경

--- src/main/java/kimp/security/SecurityConfig.java
+++ src/main/java/kimp/security/SecurityConfig.java
@@ -3,27 +3,23 @@ package kimp.security;
 import kimp.security.user.CustomAuthenticationFilter;
 import kimp.security.user.service.CustomUserDetailService;
 import kimp.user.service.UserService;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
-import org.springframework.security.web.context.SecurityContextRepository;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.List;
+import java.util.Arrays;
 
 @EnableWebSecurity
 @Configuration
@@ -38,85 +34,76 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationManager authenticationManager) throws Exception {
-        // SecurityContextRepository 설정
-        SecurityContextRepository securityContextRepository = new HttpSessionSecurityContextRepository();
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationConfiguration authConfig) throws Exception {
+        // AuthenticationManager를 AuthenticationConfiguration을 통해 가져옴
+        AuthenticationManager authenticationManager = authConfig.getAuthenticationManager();
 
         // CustomAuthenticationFilter 생성 및 설정
         CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManager);
         customAuthenticationFilter.setFilterProcessesUrl("/login");
-        customAuthenticationFilter.setSecurityContextRepository(securityContextRepository);
 
-        // CORS 설정
-        http.cors(cors -> cors.configurationSource(request -> {
-                    CorsConfiguration config = new CorsConfiguration();
-                    config.setAllowedOrigins(List.of(
-                            "http://localhost:8080",
-                            "http://localhost:3000",
-                            "http://127.0.0.1:3000",
-                            "http://127.0.0.1:8080",
-                            "localhost",
-                            "https://kimp-run-fe-jvmn-ffjqw3bd2-hojip-kims-projects.vercel.app",
-                            "https://kimp-run-fe-jvmn.vercel.app",
-                            "https://2f68-2001-2d8-ef42-eff1-a49a-9d39-ce9f-f068.ngrok-free.app"
-                    ));
-                    config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
-                    config.setAllowedHeaders(List.of("*"));
-                    config.setAllowCredentials(true);
-                    return config;
-                }))
+
+        http
                 // CSRF 비활성화
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
+                // CORS 설정 적용
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                // 세션 관리 설정
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                )
                 // 요청 권한 설정
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(
                                 "/login",
                                 "/user/sign-up",
                                 "/user",
-                                "/upbit",
                                 "/upbit/**",
-                                "/binance",
                                 "/binance/**",
-                                "/websocket",
                                 "/websocket/**",
-                                "/market",
                                 "/market/**",
-                                "market/first/**",
-                                "market/first/data",
                                 "/chat/**",
-                                "/chat/allLog",
-                                "/chat/test",
                                 "/chatService/**",
-                                "/chatService",
-                                "/category",
                                 "/category/**",
-                                "/error" // /error 추가
+                                "/error"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
-                // 세션 관리 설정
-                .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+                // 기본 폼로그인 비활성화
+                .formLogin(AbstractHttpConfigurer::disable)
+                // 로그아웃 비활성화
+                .logout(AbstractHttpConfigurer::disable)
                 // CustomAuthenticationFilter를 필터 체인에 추가
-                .addFilterAt(customAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-                // 기본 폼 로그인 비활성화
-                .formLogin(form -> form.disable());
+                .addFilterAt(customAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
 
     @Bean
-    public AuthenticationManager authenticationManager() {
-        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
-        daoAuthenticationProvider.setUserDetailsService(customUserDetailService);
-        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
-        return new ProviderManager(daoAuthenticationProvider);
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://kimp-run-fe-jvmn-ffjqw3bd2-hojip-kims-projects.vercel.app",
+                "https://kimp-run-fe-jvmn.vercel.app",
+                "https://2f68-2001-2d8-ef42-eff1-a49a-9d39-ce9f-f068.ngrok-free.app"));
+        config.setAllowedMethods(Arrays.asList("HEAD", "POST", "GET", "DELETE", "PUT", "OPTIONS"));
+        config.setAllowedHeaders(Arrays.asList("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        return new CustomUserDetailService(userService);
-    }
+    // AuthenticationManager를 직접 빈으로 등록하지 않음
+    // AuthenticationConfiguration을 통해 주입받음
+//    @Bean
+//    public AuthenticationManager authenticationManager() {
+//        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+//        daoAuthenticationProvider.setUserDetailsService(customUserDetailService);
+//        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+//        return new ProviderManager(daoAuthenticationProvider);
+//    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
