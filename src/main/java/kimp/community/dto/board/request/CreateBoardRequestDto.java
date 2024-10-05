4425770e93967610b83680a3f9e4b0efba736c6b
Hojip-Kim <rlaghwlq12@naver.com>
2024-10-05 12:54:29 +0900

    feat : CreateBoardRequestDto 구현

---
+++ src/main/java/kimp/community/dto/board/request/CreateBoardRequestDto.java
@@ -0,0 +1,13 @@
+package kimp.community.dto.board.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class CreateBoardRequestDto {
+    private String title;
+    private String content;
+}
