fee1722d618cb31032b9104570d61b36484c0633
Hojip-Kim <rlaghwlq12@naver.com>
2024-10-08 23:41:32 +0900

    feat : UserServiceImpl <-> SecurityConfig 순환참조 해결을 위한 passwordEncoder 상위참조(pacade)

--- src/main/java/kimp/config/application/AppConfig.java
+++ src/main/java/kimp/config/application/AppConfig.java
@@ -1,8 +1,10 @@
-package kimp.config;
+package kimp.config.application;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -11,5 +13,10 @@ public class AppConfig {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
