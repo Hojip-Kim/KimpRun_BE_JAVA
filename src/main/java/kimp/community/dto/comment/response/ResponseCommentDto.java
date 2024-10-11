337026646247a9de86d8d593dd81db3bd431db05
Hojip-Kim <rlaghwlq12@naver.com>
2024-10-11 23:14:55 +0900

    feat : Request/Response CreateCommentDto created

---
+++ src/main/java/kimp/community/dto/comment/response/ResponseCommentDto.java
@@ -0,0 +1,20 @@
+package kimp.community.dto.comment.response;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class ResponseCommentDto {
+    private long id;
+    private long parentCommentId;
+    private String content;
+    private int depth;
+
+    public ResponseCommentDto(long id, long parentCommentId, String content, int depth) {
+        this.id = id;
+        this.parentCommentId = parentCommentId;
+        this.content = content;
+        this.depth = depth;
+    }
+}
