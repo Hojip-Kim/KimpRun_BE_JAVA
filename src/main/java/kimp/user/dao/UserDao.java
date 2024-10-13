78c46544c6c0458f61323c2218585f31468af4fd
Hojip-Kim <rlaghwlq12@naver.com>
2024-10-13 22:19:52 +0900

    feat : UserDao update, delete logic 구현

--- src/main/java/kimp/user/dao/UserDao.java
+++ src/main/java/kimp/user/dao/UserDao.java
@@ -7,4 +7,6 @@ public interface UserDao {
     public User findUserById(Long id);
     public User findUserByLoginId(String loginId);
     public User createUser(String loginId, String password);
+    public User updateUser(User user, String newHashedPassword);
+    public Boolean deleteUser(Long id);
 }
