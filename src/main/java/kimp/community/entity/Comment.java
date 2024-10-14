2c6ec483eddb6e200c2c2d5dd3bceb451c37970d
Hojip-Kim <rlaghwlq12@naver.com>
2024-10-14 23:52:02 +0900

    refactor : Comment Entity 연관관계매핑 설

--- src/main/java/kimp/community/entity/Comment.java
+++ src/main/java/kimp/community/entity/Comment.java
@@ -1,33 +1,49 @@
 package kimp.community.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Lob;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import kimp.common.entity.TimeStamp;
 import kimp.user.entity.User;
 import lombok.Getter;
 
 @Entity
 @Getter
+@Table(name = "board_comment")
 public class Comment extends TimeStamp {
 
 
     @Column(nullable = false)
     public String content;
 
-    @ManyToOne
+    @Column(nullable = false)
+    private long parentCommentId = 0;
+
+    @Column(nullable = false)
+    private int depth = 0;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name="user_id")
     public User user;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "board_id")
     public Board board;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    public CommentLikeCount likeCount;
 
     public Comment() {
     }
 
-    public Comment(String content, User user, Board board) {
+    public Comment(User user, Board board, String content, long parentCommentId, int depth) {
         this.content = content;
         this.user = user;
         this.board = board;
+        this.parentCommentId = parentCommentId;
+        this.depth = depth;
+    }
+
+    public Comment setCommentLikeCount(CommentLikeCount commentLikeCount){
+        this.likeCount = commentLikeCount;
+        return this;
     }
 }
