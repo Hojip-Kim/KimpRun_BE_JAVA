1bcdf463dfd1baf0e49bfc500222f667220d7394
Hojip-Kim <rlaghwlq12@naver.com>
2024-11-02 12:50:21 +0900

    feat : dollar api unit-test code feature

---
+++ src/test/java/unit/kimp/market/component/DollarTest.java
@@ -0,0 +1,47 @@
+package unit.kimp.market.component;
+
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import kimp.market.dto.response.DollarResponseDto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ExtendWith(MockitoExtension.class)
+public class DollarTest {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    @Value("${dollar.api.url}")
+    private String dollarApiUrl;
+
+    @Test
+    @DisplayName("dollar 데이터 Fetch가 잘 되는지에 대한 Test.")
+    void fetchDollarData() throws IOException {
+        String data = restTemplate.getForObject(dollarApiUrl, String.class);
+        System.out.println(data);
+        DollarResponseDto dollarDto = objectMapper.readValue(data, DollarResponseDto.class);
+
+        // 데이터가 null이 아닌지 확인
+        assertNotNull(dollarDto, "DollarResponseDto 데이터가 null입니다.");
+
+        DollarResponseDto.Rates rates = dollarDto.getRates();
+        assertNotNull(rates, "Rates 데이터가 null입니다.");
+
+        // KRW 환율이 0보다 큰지 확인
+        double krwRate = dollarDto.getRates().getKRW();
+
+        assertTrue(krwRate > 0, "KRW 환율이 0보다 커야 합니다.");
+    }
+
+}
