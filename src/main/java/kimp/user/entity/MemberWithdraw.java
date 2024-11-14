f779c9054ac9988f51882bdd53ab07ef3f5af0e7
Hojip-Kim <rlaghwlq12@naver.com>
2024-11-14 20:45:05 +0900

    refactor : User -> Member Rename으로 인한 refactor

---
+++ src/main/java/kimp/user/entity/MemberWithdraw.java
@@ -0,0 +1,30 @@
+package kimp.user.entity;
+
+import jakarta.persistence.*;
+import kimp.common.entity.TimeStamp;
+import lombok.Getter;
+
+
+@Entity
+@Table(name = "member_withdraw")
+@Getter
+public class MemberWithdraw extends TimeStamp {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name="member_id")
+    private Member member;
+
+    @Column(name="is_withdraw")
+    private Boolean isWithdraw;
+
+    public MemberWithdraw() {}
+
+    public MemberWithdraw(Member member, Boolean isWithdraw) {
+        this.member = member;
+        this.isWithdraw = isWithdraw;
+    }
+}
