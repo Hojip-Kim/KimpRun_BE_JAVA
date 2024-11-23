1f68923b8771218471dfe2857b2f83f6fbab8300
Hojip-Kim <rlaghwlq12@naver.com>
2024-11-23 16:38:35 +0900

    refactor : user -> member refactor

--- src/main/java/kimp/user/entity/BannedCount.java
+++ src/main/java/kimp/user/entity/BannedCount.java
@@ -1,6 +1,5 @@
 package kimp.user.entity;
 
-
 import jakarta.persistence.*;
 import lombok.Getter;
 
@@ -9,13 +8,15 @@ import lombok.Getter;
 @Getter
 public class BannedCount {
 
+
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @OneToOne
-    @JoinColumn(name = "user_agent_id")
-    private UserAgent agent;
+    @JoinColumn(name = "member_agent_id")
+    private UserAgent memberAgent;
 
     @Column
     private Integer count;
@@ -25,7 +26,7 @@ public class BannedCount {
 
     public BannedCount(Long id, UserAgent agent, Integer count) {
         this.id = id;
-        this.agent = agent;
+        this.memberAgent = agent;
         this.count = count;
     }
 }
