2a82c336fc74e51fd9ce94349787c5c3433247e3
Hojip-Kim <rlaghwlq12@naver.com>
2024-11-23 17:06:35 +0900

    feat : createUserResponseDTO refactor (userid -> email)

--- src/main/java/kimp/user/dto/response/CreateUserResponseDto.java
+++ src/main/java/kimp/user/dto/response/CreateUserResponseDto.java
@@ -1,16 +1,16 @@
 package kimp.user.dto.response;
 
 import lombok.Getter;
-import lombok.Setter;
 
-@Setter
+
 @Getter
 public class CreateUserResponseDto {
-    public CreateUserResponseDto(String userid, String nickname) {
-        this.userid = userid;
+    private String email;
+    private String nickname;
+
+    public CreateUserResponseDto(String email, String nickname) {
+        this.email = email;
         this.nickname = nickname;
     }
 
-    private String userid;
-    private String nickname;
 }
