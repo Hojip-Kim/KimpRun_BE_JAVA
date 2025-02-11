f680eae118e1232edfd5954d122ab60ba5182f23
Hojip-Kim <rlaghwlq12@naver.com>
2025-02-11 15:05:04 +0900

    feat : oauth - google login feature

---
+++ src/main/java/kimp/security/user/OAuth2AuthenticationSuccessHandler.java
@@ -0,0 +1,43 @@
+package kimp.security.user;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.security.web.context.SecurityContextRepository;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@Slf4j
+public class OAuth2AuthenticationSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
+
+    private final SecurityContextRepository securityContextRepository = new HttpSessionSecurityContextRepository();
+
+    public OAuth2AuthenticationSuccessHandler() {
+        setDefaultTargetUrl("http://localhost:3000?login=success"); // 로그인 성공 후 이동할 기본 URL 설정
+    }
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        Authentication authentication) throws IOException, ServletException {
+        OAuth2AuthenticationToken oauth2Authentication = (OAuth2AuthenticationToken) authentication;
+        log.info("OAuth2 로그인 성공 - username: {}", oauth2Authentication.getName());
+
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+        context.setAuthentication(authentication);
+        SecurityContextHolder.setContext(context);
+
+        securityContextRepository.saveContext(context, request, response);
+
+        super.onAuthenticationSuccess(request, response, authentication);
+    }
+}
