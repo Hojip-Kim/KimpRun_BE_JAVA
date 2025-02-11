4c5db7f369ba7fb86c1678ce96085940bd656ec1
Hojip-Kim <rlaghwlq12@naver.com>
2025-02-11 15:11:02 +0900

    feat : bithumb notice dto feature

---
+++ src/main/java/kimp/scrap/dto/bithumb/BithumbNoticeDto.java
@@ -0,0 +1,11 @@
+package kimp.scrap.dto.bithumb;
+
+import kimp.scrap.dto.common.ExchangeNoticeDto;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class BithumbNoticeDto extends ExchangeNoticeDto<String> {
+
+}
