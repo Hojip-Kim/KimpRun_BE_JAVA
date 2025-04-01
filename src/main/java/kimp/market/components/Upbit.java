8ec2d5653b6b7824efe3af5f64bb0e8ca1ed986d
Hojip-Kim <rlaghwlq12@naver.com>
2025-04-01 23:45:57 +0900

    refactor : dto 도메인 분리로 인한 import refactor

--- src/main/java/kimp/market/components/Upbit.java
+++ src/main/java/kimp/market/components/Upbit.java
@@ -2,11 +2,11 @@ package kimp.market.components;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.annotation.PostConstruct;
-import kimp.market.dto.response.MarketList;
+import kimp.market.dto.market.response.MarketList;
 import kimp.market.common.MarketCommonMethod;
-import kimp.market.dto.response.MarketDataList;
-import kimp.market.dto.response.UpbitMarketList;
-import kimp.market.dto.response.UpbitTicker;
+import kimp.market.dto.market.response.MarketDataList;
+import kimp.market.dto.market.response.UpbitMarketList;
+import kimp.market.dto.market.response.UpbitTicker;
 import kimp.websocket.dto.response.UpbitDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
