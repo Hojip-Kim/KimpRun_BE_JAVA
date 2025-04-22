27281e0a7e6e41942273fbcaad8bcab5f8b4907e
Hojip-Kim <rlaghwlq12@naver.com>
2025-04-22 14:15:04 +0900

    feat : coin service feature - 스케줄러를 통해 코인이 생기거나, 필요없어질 때 데이터베이스 증/감 메소드 구현 - 코인 벌크 삽입 구현 - exchange id에따라 코인리스트 가져오기 구

--- src/main/java/kimp/market/service/CoinService.java
+++ src/main/java/kimp/market/service/CoinService.java
@@ -1,14 +1,24 @@
 package kimp.market.service;
 
+import kimp.market.dto.coin.common.ChangeCoinDto;
 import kimp.market.dto.coin.request.*;
 import kimp.market.dto.coin.response.CoinResponseDto;
 import kimp.market.dto.coin.response.CoinResponseWithMarketTypeDto;
+
+import java.util.List;
 
 public interface CoinService {
 
     public CoinResponseWithMarketTypeDto getCoinByID(long id);
 
     public CoinResponseDto createCoin(CreateCoinDto createCoinDto);
+
+    // Binance / Upbit 스케줄러를통해 지워지거나 새로 생긴 코인을 추가하거나 지웁니다.
+    public void createWithDeleteCoin(ChangeCoinDto changeCoinDto);
+
+    public List<CoinResponseDto> createCoinBulk(List<CreateCoinDto> createCoinDtos);
+
+    public List<CoinResponseDto> getCoinsByExchangeId(long exchangeId);
 
     public CoinResponseDto updateContentCoin(UpdateContentCoinDto updateCoinDto);
 
--- src/main/java/kimp/market/service/serviceImpl/CoinServiceImpl.java
+++ src/main/java/kimp/market/service/serviceImpl/CoinServiceImpl.java
@@ -6,6 +6,7 @@ import kimp.exchange.entity.Exchange;
 import kimp.market.Enum.MarketType;
 import kimp.market.dao.CoinDao;
 import kimp.market.dao.CoinExchangeDao;
+import kimp.market.dto.coin.common.ChangeCoinDto;
 import kimp.market.dto.coin.request.*;
 import kimp.market.dto.coin.response.CoinResponseDto;
 import kimp.market.dto.coin.response.CoinResponseWithMarketTypeDto;
@@ -15,8 +16,10 @@ import kimp.market.service.CoinService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 public class CoinServiceImpl implements CoinService {
@@ -25,13 +28,13 @@ public class CoinServiceImpl implements CoinService {
     private final DtoConverter dtoConverter;
     private final ExchangeDao exchangeDao;
     private final CoinExchangeDao coinExchangeDao;
+
     public CoinServiceImpl(CoinDao coinDao, DtoConverter dtoConverter, ExchangeDao exchangeDao, CoinExchangeDao coinExchangeDao) {
         this.coinDao = coinDao;
         this.dtoConverter = dtoConverter;
         this.exchangeDao = exchangeDao;
         this.coinExchangeDao = coinExchangeDao;
     }
-
 
     @Override
     @Transactional
@@ -53,18 +56,127 @@ public class CoinServiceImpl implements CoinService {
         for(Exchange exchange : exchanges) {
             CoinExchange coinExchange = new CoinExchange(coin, exchange);
             coin.addCoinExchanges(coinExchange);
-            coinExchange.getExchange().addCoinExchanges(coinExchange);
+            exchange.addCoinExchanges(coinExchange);
         }
 
         return dtoConverter.convertCoinToCoinResponseDto(coin);
     }
 
     @Override
+    public void createWithDeleteCoin(ChangeCoinDto changeCoinDto) {
+        List<String> listedCoinSymbol = changeCoinDto.getListedSymbols();
+        List<String> delistedCoinSymbol = changeCoinDto.getDelistedSymbols();
+
+        List<MarketType> marketTypes = new ArrayList<>();
+        List<Long> exchangeIds = new ArrayList<>();
+
+        marketTypes.add(changeCoinDto.getType());
+        Exchange exchange = exchangeDao.getExchangeByMarketType(changeCoinDto.getType());
+
+        exchangeIds.add(exchange.getId());
+
+        if(!listedCoinSymbol.isEmpty()){
+            for(String symbol : listedCoinSymbol){
+                CreateCoinDto createCoinDto = new CreateCoinDto(symbol, null, null, marketTypes);
+                createCoin(createCoinDto);
+            }
+        }
+        if(!delistedCoinSymbol.isEmpty()){
+            marketTypes.add(changeCoinDto.getType());
+            for(String symbol : delistedCoinSymbol){
+                Coin coin = coinDao.getCoinBySymbol(symbol);
+                AdjustExchangeCoinDto adjustExchangeCoinDto = new AdjustExchangeCoinDto(coin.getId(), exchangeIds);
+                deleteExchangeCoin(adjustExchangeCoinDto);
+            }
+        }
+    }
+
+    @Override
+    @Transactional
+    public List<CoinResponseDto> createCoinBulk(List<CreateCoinDto> createCoinDtos) {
+
+        // 들어온 DTO 전체에서 symbols 뽑기
+        List<String > symbols = createCoinDtos
+                .stream().map(CreateCoinDto::getSymlbol)
+                .toList();
+
+        // 한번에 fetch-join해서 기존코인 + 연관된 coinExchange , Exchange 모두 로드
+        List<Coin> existingCoins = coinDao.findWithExchangesBySymbols(symbols);
+
+        // 기존에 있던 심볼만 set으로
+        Map<String, Coin> existingMap = existingCoins.stream()
+                .collect(Collectors.toMap(Coin::getSymbol, Function.identity()));
+
+        // 모든 MarketType에 해당하는 Exchange 한번씩 조회 (N+1방지용)
+        Set<MarketType> allMarkets = createCoinDtos.stream()
+                .flatMap(dto -> dto.getMarketType().stream())
+                .collect(Collectors.toSet());
+        List<Exchange> allExchanges = exchangeDao.getExchangeByMarketTypes(new ArrayList<>(allMarkets));
+        Map<MarketType, List<Exchange>> exchangeMap = allExchanges.stream()
+                .collect(Collectors.groupingBy(Exchange::getMarket));
+
+        // 신규 생성할 DTO와 기존 업데이트 할 DTO를 분리
+
+        List<CreateCoinDto> toCreateCoinDtos = createCoinDtos.stream()
+                .filter(dto -> !existingMap.containsKey(dto.getSymlbol()))
+                .toList();
+        List<CreateCoinDto> toUpdate = createCoinDtos.stream()
+                .filter(dto -> existingMap.containsKey(dto.getSymlbol()))
+                .toList();
+
+        // 신규 코인 생성
+        List<Coin> newCoins = toCreateCoinDtos.stream()
+                .map(dto -> {
+                    Coin coin = new Coin(dto.getSymlbol(), dto.getName(), dto.getEnglishName());
+                    dto.getMarketType().stream()
+                            .flatMap(marketType -> exchangeMap.getOrDefault(marketType, List.of()).stream())
+                            .forEach(exchange -> {
+                                CoinExchange coinExchange = new CoinExchange(coin, exchange);
+                                coin.addCoinExchanges(coinExchange);
+                                exchange.addCoinExchanges(coinExchange);
+                            });
+                    return coin;
+                }).toList();
+
+        // 기존 Coin에 없는 CoinExchange만 붙이기 (신규마켓만 붙이기)
+        // 이미 코인은 영속화된 상태이므로 더티체킹으로 처리됨. (새로운 select문 생기지않음)
+        for(CreateCoinDto dto : toUpdate) {
+            Coin coin = existingMap.get(dto.getSymlbol());
+            Set<MarketType> existMarkets = new HashSet<>(coin.getMarketTypes());
+            dto.getMarketType().stream()
+                    .filter(marketType -> !existMarkets.contains(marketType))
+                    .flatMap(marketType -> exchangeMap.getOrDefault(marketType, List.of()).stream())
+                    .forEach(exchange -> {
+                        CoinExchange coinExchange = new CoinExchange(coin, exchange);
+                        coin.addCoinExchanges(coinExchange);
+                        exchange.addCoinExchanges(coinExchange);
+                    });
+        }
+
+        if(!newCoins.isEmpty()) {
+            coinDao.createCoinBulk(newCoins);
+        }
+
+        return dtoConverter.convertCoinListToCoinResponseDtoList(
+                Stream.concat(existingCoins.stream(), newCoins.stream())
+                        .toList()
+        );
+    }
+
+    // coin 가져올 때 select 한번으로 가져오므로 N+1 발생안함
+    @Override
+    public List<CoinResponseDto> getCoinsByExchangeId(long exchangeId) {
+
+        List<Coin> coinList = coinDao.getCoinsByExchangeId(exchangeId);
+
+        return dtoConverter.convertCoinListToCoinResponseDtoList(coinList);
+    }
+
+    @Override
     @Transactional
     public CoinResponseDto updateContentCoin(UpdateContentCoinDto updateCoinDto) {
-        Coin coin = coinDao.findById(updateCoinDto.getId());
 
-        return dtoConverter.convertCoinToCoinResponseDto(coin.writeContent(updateCoinDto.getContent()));
+        return dtoConverter.convertCoinToCoinResponseDto(coinDao.updateContentCoin(updateCoinDto.getId(), updateCoinDto.getContent()));
 
     }
 
