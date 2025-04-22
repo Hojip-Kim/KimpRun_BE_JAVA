fa73e32b1878809f1ad423d137a0c7afca4c213e
Hojip-Kim <rlaghwlq12@naver.com>
2025-04-22 14:46:32 +0900

    feat : 서비스 내부(외부에 공개되지않는)에서 사용하는 코인리스트 제공 dto feature

---
+++ src/main/java/kimp/market/dto/coin/common/ServiceCoinWrapperDto.java
@@ -0,0 +1,25 @@
+package kimp.market.dto.coin.common;
+
+import kimp.market.Enum.MarketType;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+public class ServiceCoinWrapperDto {
+
+    private MarketType marketType;
+
+    private List<ServiceCoinDto> serviceCoins;
+
+    public ServiceCoinWrapperDto(MarketType marketType, List<ServiceCoinDto> serviceCoins) {
+        this.marketType = marketType;
+
+        if(serviceCoins.isEmpty()){
+            throw new IllegalArgumentException("serviceCoins is empty");
+        }
+        this.serviceCoins = serviceCoins;
+    }
+}
