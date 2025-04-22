36a75664526d277bb844db5d9b4bcf525a79d757
Hojip-Kim <rlaghwlq12@naver.com>
2025-04-22 14:25:49 +0900

    feat : market에 관련된 메서드를 정의하는 MarketMethod 컴포넌트 feature

---
+++ src/main/java/kimp/common/method/MarketMethod.java
@@ -0,0 +1,44 @@
+package kimp.common.method;
+
+import kimp.exchange.component.impl.exchange.BinanceScrap;
+import kimp.exchange.component.impl.exchange.BithumbScrap;
+import kimp.exchange.component.impl.exchange.CoinoneScrap;
+import kimp.exchange.component.impl.exchange.UpbitScrap;
+import kimp.market.Enum.MarketType;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MarketMethod {
+
+    @Value("${upbit.notice.detail.url}")
+    private String upbitAbsoluteUrl;
+    @Value("${bithumb.notice.detail.url}")
+    private String bithumbAbsoluteUrl;
+    @Value("${coinone.notice.detail.url}")
+    private String coinoneAbsoluteUrl;
+    @Value("${binance.notice.detail.url}")
+    private String binanceAbsoluteUrl;
+
+    public static Class getMarketClass(MarketType marketType){
+        return switch (marketType) {
+            case UPBIT -> UpbitScrap.class;
+            case BINANCE -> BinanceScrap.class;
+            case COINONE -> CoinoneScrap.class;
+            case BITHUMB -> BithumbScrap.class;
+            default -> throw new IllegalArgumentException("Unsupported market type: " + marketType);
+        };
+    }
+
+    public String getMarketAbsoluteUrlByMarketType(MarketType marketType){
+        return switch (marketType){
+            case UPBIT -> this.upbitAbsoluteUrl;
+            case BITHUMB -> this.bithumbAbsoluteUrl;
+            case COINONE -> this.coinoneAbsoluteUrl;
+            case BINANCE -> this.binanceAbsoluteUrl;
+            default -> throw new IllegalArgumentException("Unsupported market type: " + marketType);
+        };
+    }
+
+
+}
