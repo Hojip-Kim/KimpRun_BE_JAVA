066163538ddce66478b9d3d0e6133a3d12204a48
Hojip-Kim <rlaghwlq12@naver.com>
2025-04-22 14:40:09 +0900

    feat : notice c/r/u/d 메서드 feature

---
+++ src/main/java/kimp/exchange/dao/NoticeDao.java
@@ -0,0 +1,24 @@
+package kimp.exchange.dao;
+
+import kimp.exchange.entity.Notice;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface NoticeDao {
+
+    public Notice createNotice(Notice notice);
+
+    public boolean createBulkNotice(List<Notice> notices);
+
+    public Notice getNotice(long id);
+
+    public Notice getNoticeByLink(String link);
+
+    public void deleteNotice(long id);
+
+    public Page<Notice> findByExchangeOrderByRegistedAtDesc(long exchangeId, Pageable pageable);
+
+
+}
