e236a439a31df00f7589abcd2ad75bcfa60f6e5f
Hojip-Kim <rlaghwlq12@naver.com>
2025-06-11 23:46:44 +0900

    chore : scrap service 메서드 chore (notice service 사용에 따라 더이상 사용되지 않음)

--- src/main/java/kimp/exchange/service/ScrapService.java
+++ src/main/java/kimp/exchange/service/ScrapService.java
@@ -1,11 +1,5 @@
 package kimp.exchange.service;
 
-import kimp.exchange.dto.notice.NoticeParsedData;
-
-import java.util.List;
-
 public interface ScrapService {
-
-    List<List<NoticeParsedData>> getNotices();
 
 }
