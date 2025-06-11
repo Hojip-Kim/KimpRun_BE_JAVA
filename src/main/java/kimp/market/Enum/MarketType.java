f16514c9717beb679d36447b044e0951d184926c
Hojip-Kim <rlaghwlq12@naver.com>
2025-06-11 23:36:18 +0900

    #33 feat: dto converter exchange url 필드 추가에 따른 로직 추가

--- src/main/java/kimp/market/Enum/MarketType.java
+++ src/main/java/kimp/market/Enum/MarketType.java
@@ -5,19 +5,31 @@ import lombok.Getter;
 @Getter
 public enum MarketType {
 
-    BINANCE("BINANCE", "https://www.binance.com/"),
-    UPBIT("UPBIT", "https://upbit.com/home"),
-    COINONE("COINONE", "https://www.coinone.com/"),
-    BITHUMB("BITHUMB", "https://www.bithumb.com/"),
+    ALL("ALL", "", 1, ""),
+    BINANCE("BINANCE", "https://www.binance.com/", 2, "https://www.binance.com/en/support/announcement/detail/"),
+    UPBIT("UPBIT", "https://upbit.com/home", 3, "https://upbit.com/service_center/notice?id="),
+    COINONE("COINONE", "https://www.coinone.com/", 4, "https://coinone.co.kr"),
+    BITHUMB("BITHUMB", "https://www.bithumb.com/", 5, "https://feed.bithumb.com"),
     ;
 
     private final String MarketName;
     private final String mainUrl;
+    private final long id;
+    private final String noticeUrl;
 
-    MarketType(String MarketName, String url) {
+    MarketType(String MarketName, String url, long id, String noticeUrl) {
         this.MarketName = MarketName;
         this.mainUrl = url;
+        this.id = id;
+        this.noticeUrl = noticeUrl;
     }
 
-
+    public static MarketType getMarketTypeByExchangeId(long id){
+        for(MarketType marketType : MarketType.values()){
+            if(marketType.getId() == id){
+                return marketType;
+            }
+        }
+        return null;
+    }
 }
