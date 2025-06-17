a8f80330ebec2787e023604fdce50f07cefc0f0a
Hojip-Kim <rlaghwlq12@naver.com>
2025-06-17 23:39:59 +0900

    refactor : binance component 로직 변경 1. 기존 uri가 너무 길어 전부 요청하지 못하던 점 해결 - 바이낸스의 500개 가량 되는 데이터를 모두 가져올 수 있도록 변경

--- src/main/java/kimp/market/components/impl/market/Binance.java
+++ src/main/java/kimp/market/components/impl/market/Binance.java
@@ -1,18 +1,21 @@
-package kimp.market.components;
+package kimp.market.components.impl.market;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.annotation.PostConstruct;
 import kimp.market.Enum.MarketType;
 import kimp.market.common.MarketCommonMethod;
+import kimp.market.components.CombineMarketListProvider;
+import kimp.market.components.MarketListProvider;
+import kimp.market.components.impl.Market;
 import kimp.market.dto.coin.common.ChangeCoinDto;
 import kimp.market.dto.coin.common.ServiceCoinDto;
 import kimp.market.dto.coin.common.ServiceCoinWrapperDto;
-import kimp.market.dto.market.response.BinanceMarketList;
+import kimp.market.dto.coin.common.crypto.BinanceCryptoDto;
 import kimp.market.dto.market.response.BinanceTicker;
 import kimp.market.dto.market.response.MarketDataList;
-import kimp.market.dto.market.response.MarketList;
+import kimp.market.dto.market.common.MarketList;
 import kimp.market.service.CoinService;
-import kimp.websocket.dto.response.BinanceDto;
+import kimp.market.dto.coin.common.market.BinanceDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -26,12 +29,11 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 @Component
 @Slf4j
 @Qualifier("binance")
-public class Binance extends Market {
+public class Binance extends Market<BinanceCryptoDto> {
 
     private final MarketCommonMethod marketCommonMethod;
     private final RestTemplate restTemplate;
@@ -42,17 +44,9 @@ public class Binance extends Market {
 
     /**
      * binance의 market List.
-     * Market Data에 "USDT"가 붙어있는 상태의 리스트입니다.
-     * 예를들어, ["BTCUSDT", "ETHUSDT"] 형태의 데이터입니다.
+     * binanceMarketList에 market name들을 저장해둡니다
      */
-    public MarketList binanceMarketList = null;
-
-    /**
-     * binance의 market List.
-     * "USDT"가 붙어있지 않은 상태의 리스트입니다.
-     * 예를들어, ["BTC", "ETH"] 형태의 데이터입니다.
-     */
-    public MarketList binanceMarketPair = null;
+    public MarketList<BinanceCryptoDto> binanceMarketList = null;
 
     public MarketDataList<BinanceDto> binanceMarketDataList;
 
@@ -83,32 +77,25 @@ public class Binance extends Market {
      * @return binance의 market List들을 return합니다.
      *         여기서 return되는 market List들은 "USDT"가 빠진 데이터입니다.
      *         즉, ["BTC", "ETH"] 등과같은 형태로 return이 됩니다.
-     * @throws IOException
      */
     @Override
-    public MarketList getMarketList(){
+    public MarketList<BinanceCryptoDto> getMarketList(){
         return this.binanceMarketList;
     }
 
     @Override
-    public MarketList getMarketPair(){
-        return this.binanceMarketPair;
-    }
-
-    @Override
     public ServiceCoinWrapperDto getServiceCoins(){
-        MarketList marketList = getMarketPair();
-        List<String> stringMarketList = marketList.getMarkets();
+        List<String> stringMarketPair = getMarketList().getPairList();
 
         List<ServiceCoinDto> serviceCoinDtos = new ArrayList<>();
 
-        for(String market : stringMarketList){
-            serviceCoinDtos.add(new ServiceCoinDto(market, null, market));
+        // 여기서 영어이름 넣어줘야함
+        for(String pair : stringMarketPair){
+            serviceCoinDtos.add(new ServiceCoinDto(pair, null, pair));
         }
 
         return new ServiceCoinWrapperDto(this.getMarketType(), serviceCoinDtos);
     }
-
     @Override
     public MarketType getMarketType() {
         return MarketType.BINANCE;
@@ -128,38 +115,42 @@ public class Binance extends Market {
             throw new IllegalArgumentException("Binance Market List is null");
         }
 
-        String requestStringURL = binanceTickerUrl + "[" +
-                binanceMarketList.getMarkets().stream()
-                        .map(market -> "\"" + market + "\"")
-                        .collect(Collectors.joining(","))
-                + "]";
-        BinanceTicker[] tickerData = restTemplate.getForObject(requestStringURL, BinanceTicker[].class);
+        List<String> binanceCryptoList = binanceMarketList.getCryptoList();
 
-        BinanceDto binanceDto = null;
-        MarketDataList<BinanceDto> binanceMarketDataList = null;
+        int maxIndex = binanceCryptoList.size() / 100 + 1;
+
+        String[][] binanceCryptoListSplit = new String[maxIndex][];
+        for(int i = 0; i < maxIndex; i++){
+            if(i == maxIndex - 1){
+                binanceCryptoListSplit[i] = binanceCryptoList.subList(i * 100, binanceCryptoList.size()).toArray(new String[0]);
+            }else{
+                binanceCryptoListSplit[i] = binanceCryptoList.subList(i * 100, (i + 1) * 100).toArray(new String[0]);
+            }
+        }
 
         List<BinanceDto> marketDataList = new ArrayList<>();
-        String rateChange = "";
-        for (int i = 0; i < tickerData.length; i++) {
-            if(tickerData[i].getPriceChangePercent().compareTo(BigDecimal.ZERO) < 0){
-                rateChange = "FALL";
-            }else if(tickerData[i].getPriceChangePercent().compareTo(BigDecimal.ZERO) > 0){
-                rateChange = "RISE";
-            }else{
-                rateChange = "EVEN";
-            }
 
-            binanceDto = new BinanceDto(tickerData[i].getSymbol().replace("USDT", ""), tickerData[i].getQuoteVolume(), tickerData[i].getPriceChangePercent(), tickerData[i].getHighPrice(), tickerData[i].getLowPrice(), tickerData[i].getOpenPrice(), tickerData[i].getLastPrice(), rateChange, tickerData[i].getVolume());
-            marketDataList.add(binanceDto);
+        for(String[] binanceCrypto : binanceCryptoListSplit){
+            String requestUrl = binanceTickerUrl + "[\"" + String.join("\",\"", binanceCrypto) + "\"]";
+            BinanceTicker[] tickerData = restTemplate.getForObject(requestUrl, BinanceTicker[].class);
+
+            BinanceDto binanceDto = null;
+
+            String rateChange = "";
+            for (int i = 0; i < tickerData.length; i++) {
+                if(tickerData[i].getPriceChangePercent().compareTo(BigDecimal.ZERO) < 0){
+                    rateChange = "FALL";
+                }else if(tickerData[i].getPriceChangePercent().compareTo(BigDecimal.ZERO) > 0){
+                    rateChange = "RISE";
+                }else{
+                    rateChange = "EVEN";
+                }
+                binanceDto = new BinanceDto(tickerData[i].getSymbol().replace("USDT", ""), tickerData[i].getQuoteVolume(), tickerData[i].getPriceChangePercent(), tickerData[i].getHighPrice(), tickerData[i].getLowPrice(), tickerData[i].getOpenPrice(), tickerData[i].getLastPrice(), rateChange, tickerData[i].getVolume());
+                marketDataList.add(binanceDto);
+            }
         }
 
         binanceMarketDataList = new MarketDataList<>(marketDataList);
-        if(binanceMarketDataList != null){
-            this.binanceMarketDataList = binanceMarketDataList;
-        }else{
-            throw new IllegalArgumentException("Binance Market List is null");
-        }
-
     }
 
     private List<String> getBinanceMarketListMatchedInUpbit() throws IOException {
@@ -168,39 +159,22 @@ public class Binance extends Market {
         return this.combineMarketListProvider.getMarketCombineList(binanceMarketList, upbitMarketList);
     }
 
-    public List<String> detachUsdtTicker(List<String> marketList){
-
-        return marketList.stream().map(market -> market + "USDT").collect(Collectors.toList());
-    }
-
     /**
      * Market List를 갱신합니다.
-     * binance의 Market List를 "USDT"를 제거하고 갱신합니다.
-     * 예를들어, ["BTC", "ETH"] 의 형태로 갱신합니다.
-     *
      * @throws IOException
      */
     public void setBinanceMarketList() throws IOException {
-        // marketList : 바이낸스의 마켓이름 데이터
-
-        // 아래 해당하는 binanceMarketListProvider의 marketList는 binance의 모든 ticker에 대한 데이터값을 받아올 수 있습니다.
-        // 단, binacne측 nginx 설정으로 인해 request uri가 너무 길다는 에러를 반환하므로, 현재는 사용되지 않습니다.
-//         List<String> marketList = binanceMarketListProvider.getMarketListWithTicker();
-
         // upbit와 연관이 있는 마켓리스트만 호출합니다.
-        List<String> marketList = detachUsdtTicker(getBinanceMarketListMatchedInUpbit());
+        List<String> marketList = binanceMarketListProvider.getMarketListWithTicker();
 
-        List<String> marketPair = new ArrayList();
+        MarketList<BinanceCryptoDto> marketDtoList = new MarketList<>(new ArrayList<BinanceCryptoDto>());
 
-        for (int i = 0; i < marketList.size(); i++) {
-            String modifiedString = marketList.get(i).replace("USDT", "");
-            marketPair.add(modifiedString);
+        for(String market : marketList){
+            String replacedMarket = market.replace("USDT", "");
+            marketDtoList.getCryptoDtoList().add(new BinanceCryptoDto(replacedMarket, "USDT"));
         }
 
-        // "USDT"가 붙은 market List
-        this.binanceMarketList = new BinanceMarketList(marketList);
-        // "USDT"가 빠진 market pair
-        this.binanceMarketPair = new BinanceMarketList(marketPair);
+        this.binanceMarketList = marketDtoList;
     }
 
     /**
@@ -210,15 +184,15 @@ public class Binance extends Market {
      */
     @Scheduled(fixedRate = 60 * 1000)
     public void scheduledSetupBinanceMarketData() throws IOException {
-        MarketList prevMarketPair = getMarketPair();
+        MarketList<BinanceCryptoDto> prevMarketPair = getMarketList();
         setBinanceMarketList();
-        MarketList nextMarketPair = getMarketPair();
+        MarketList<BinanceCryptoDto> nextMarketPair = getMarketList();
 
         // 만약 이전과, 이후의 객체가 다르면 바뀐것
         if(!prevMarketPair.equals(nextMarketPair)){
-            List<String> prevMarketList = prevMarketPair.getMarkets();
+            List<String> prevMarketList = prevMarketPair.getPairList();
             Set<String> prevMarketSet = new HashSet<>(prevMarketList);
-            List<String> nextMarketList = nextMarketPair.getMarkets();
+            List<String> nextMarketList = nextMarketPair.getPairList();
             Set<String> nextMarketSet = new HashSet<>(nextMarketList);
 
             List<String> listCoinSymbols = new ArrayList<>();
