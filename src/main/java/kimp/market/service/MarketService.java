935023d289a07214714ebe26729f91fa481028e0
Hojip-Kim <rlaghwlq12@naver.com>
2025-06-22 13:07:24 +0900

    #40 feat : 두 거래소 간 교차 데이터(코인) 반환 로직 추가 & 거래소 추가(bithumb, coinone)

--- src/main/java/kimp/market/service/MarketService.java
+++ src/main/java/kimp/market/service/MarketService.java
@@ -2,21 +2,25 @@ package kimp.market.service;
 
 import kimp.market.Enum.MarketType;
 import kimp.market.dto.coin.common.ServiceCoinWrapperDto;
+import kimp.market.dto.market.common.MarketList;
 import kimp.market.dto.market.response.CombinedMarketList;
 import kimp.market.dto.market.response.CombinedMarketDataList;
 import kimp.market.dto.market.response.MarketDataList;
 
 import java.io.IOException;
+import java.util.List;
 
 public interface MarketService {
 
     public ServiceCoinWrapperDto getCoinListFromExchange(MarketType marketType);
 
-    public CombinedMarketList getMarketList(String firstMarket, String secondMarket) throws IOException;
+    public CombinedMarketList getMarketList(MarketType firstMarket, MarketType secondMarket);
 
-    public CombinedMarketDataList getCombinedMarketDataList(String firstMarket, String secondMarket) throws IOException;
+    public CombinedMarketDataList getCombinedMarketDataList(MarketType firstMarket, MarketType secondMarket);
 
-    public MarketDataList getMarketDataList(String market) throws IOException;
+    public List<String> getCombineMarketList(MarketList firstMarketList, MarketList secondMarketList);
+
+    public MarketDataList getMarketDataList(MarketType market) throws IOException;
 
     public void getMarkets() throws IOException;
 }
