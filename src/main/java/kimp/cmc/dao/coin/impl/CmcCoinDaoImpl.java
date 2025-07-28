65ab8bc9208aeb42365d25c0315cba6c964ec971
Hojip-Kim <rlaghwlq12@naver.com>
2025-07-28 16:10:31 +0900

    #40 feat : CmcCoinDao 메서드명추가 - Mainnets 찾기 - Platforms 찾기 - CmcCoin 관련 데이터들 찾기

--- src/main/java/kimp/cmc/dao/coin/impl/CmcCoinDaoImpl.java
+++ src/main/java/kimp/cmc/dao/coin/impl/CmcCoinDaoImpl.java
@@ -2,6 +2,8 @@ package kimp.cmc.dao.coin.impl;
 
 import kimp.cmc.dao.coin.CmcCoinDao;
 import kimp.cmc.entity.coin.CmcCoin;
+import kimp.cmc.entity.coin.CmcMainnet;
+import kimp.cmc.entity.coin.CmcPlatform;
 import kimp.cmc.repository.coin.CmcCoinRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,5 +28,25 @@ public class CmcCoinDaoImpl implements CmcCoinDao {
         }else{
             return true;
         }
+    }
+
+    @Transactional
+    public CmcCoin findCmcCoinById(Long id){
+        return this.cmcCoinRepository.findById(id).orElse(null);
+    }
+    
+    @Transactional(readOnly = true)
+    public CmcCoin findCmcCoinByIdWithOneToOneRelations(Long id){
+        return this.cmcCoinRepository.findByIdWithOneToOneRelations(id).orElse(null);
+    }
+    
+    @Transactional(readOnly = true)
+    public List<CmcMainnet> findMainnetsByCmcCoinId(Long cmcCoinId){
+        return this.cmcCoinRepository.findMainnetsByCmcCoinId(cmcCoinId);
+    }
+    
+    @Transactional(readOnly = true)
+    public List<CmcPlatform> findPlatformsByCmcCoinId(Long cmcCoinId){
+        return this.cmcCoinRepository.findPlatformsByCmcCoinId(cmcCoinId);
     }
 }
